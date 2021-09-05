aa5c694bdd4df44bf5429938010b1226c66f8883
bhwong11 <user@email.com>
2021-09-04 23:04:41 -0400

    added hello world h1 to page

--- artist/models.py
+++ artist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import Model, CharField, PositiveIntegerField, TextField,DateTimeField
+from django.db.models.deletion import CASCADE
+
+class Artwork(Model):
+    title = CharField(max_length=144, unique=True)
+    image = CharField(max_length=1000)
+    description = TextField(max_length=1000)
+    published_date = DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['published_date']
--- artist/views.py
+++ artist/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.views import View
+from django.views.generic.base import TemplateView
+
+from .models import Artwork
 
 # Create your views here.
+class Home(View):
+    def get(self,request):
+        return render(request,'home.html')
+
