31bcdff1153848c2bcc1cc3704ef753c524efe2b
bhwong11 <user@email.com>
2021-09-17 10:44:44 -0400

    test check

--- artist_profile/settings.py
+++ artist_profile/settings.py
@@ -124,7 +124,7 @@ ROOT_URLCONF = 'artist_profile.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'artist', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
